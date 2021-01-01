ff058f29ee15391d42176b26863e11618524c153
Chamon562 <Chamon562@gmail.com>
2021-01-01 13:30:45 -0800

    initial setup to push existing pre setup to already made github.

---
+++ README.md
@@ -0,0 +1,2 @@
+# guess_number_js
+This is a small guessing game for users to see how many tries it takes to get the right answer. Done with Javascript.
---
+++ app.js
@@ -0,0 +1,4 @@
+// need to make a Play Game function
+// show how the guess is too high or too low when the user hits check me
+// show the guess is correct 
+// show history and its stored 
